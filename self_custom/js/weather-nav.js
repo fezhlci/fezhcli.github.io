9d194a8dd313480666d591acbb511cd82e301f23
fezhlci <1431382669@qq,com>
2023-03-21 19:50:33 +0800

    Site updated: 2023-03-21 19:48:46

---
+++ self_custom/js/weather-nav.js
@@ -0,0 +1,23 @@
+WIDGET = {
+	"CONFIG": {
+		"modules": "0124",
+		"background": "5",
+		"tmpColor": "000000",
+		"tmpSize": "14",
+		"cityColor": "000000",
+		"citySize": "14",
+		"aqiColor": "000000",
+		"aqiSize": "14",
+		"weatherIconSize": "18",
+		"alertIconSize": "18",
+		"padding": "10px 10px 10px 10px",
+		"shadow": "0",
+		"language": "auto",
+		"borderRadius": "8",
+		"fixed": "false",
+		"vertical": "right",
+		"horizontal": "left",
+		"key": "4bf1b91a38874c01abc8ec6258cd108e"
+	}
+}
+
